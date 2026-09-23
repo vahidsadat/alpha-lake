b0eee54526b9fe729afac8c84fbd1f164c239a32
vahidsadat <sadatvahid1372@gmail.com>
2026-09-23 16:35:34 +0200

    obtain all desire information from broze and store in spark dataframe

--- src/silver/transform_sec_facts.py
+++ src/silver/transform_sec_facts.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from pyspark.sql import SparkSession, functions
+from pyspark.sql import SparkSession, functions as F
 
 spark = SparkSession.builder.appName(
     name="AlphaLake"
@@ -8,12 +8,29 @@ spark = SparkSession.builder.appName(
 def read_json(ticker:str):
     path = Path(f"data/bronze/sec/{ticker.upper()}/companyfacts.json")
 
-    df_spark = (spark.read.option("multiline","true").json(str(path)))
-
-    df_spark.printSchema()
-    selected_df = df_spark.select(
-        functions.explode("facts.us-gaap.AccountsPayableCurrent.units.USD").alias("observation")
-    )
+    raw_df = spark.read.text(
+        str(path),
+        wholetext=True)
     
-    selected_df.select("observation.*").show()
+    variant_df = raw_df.select( F.parse_json("value").alias("document") )
+    us_gaap_df = variant_df.select(F.variant_get(F.col("document"),"$.facts['us-gaap']","variant").alias("us_gaap"))
+    concept_df = us_gaap_df.lateralJoin(spark.tvf.variant_explode((F.col("us_gaap")).outer()).alias("concept"))
+    units_df = concept_df.select(F.variant_get(F.col("concept.value"),"$.units","variant").alias("units_info"), "concept.key")
+    observations_info_df = units_df.lateralJoin(spark.tvf.variant_explode((F.col("units_info")).outer()).alias("observations_info"))
+    observations_info_df = observations_info_df.select(F.col("concept.key").alias( "concept"),F.col("observations_info.key").alias( "unit"), F.col("observations_info.value").alias( "observations"))
+    observations_df = observations_info_df.lateralJoin(spark.tvf.variant_explode((F.col("observations")).outer()).alias("observations_attr"))
+    fields_df = observations_df.select(
+        F.lit(ticker).alias("ticker"),"concept", "unit",
+        F.variant_get(F.col("observations_attr.value"),"$.accn","string").alias("accession_number"),
+        F.variant_get(F.col("observations_attr.value"),"$.start","date").alias("period_start_date"),
+        F.variant_get(F.col("observations_attr.value"),"$.end","date").alias("period_end_date"),
+        F.variant_get(F.col("observations_attr.value"),"$.filed","date").alias("filing_date"),
+        F.variant_get(F.col("observations_attr.value"),"$.form","string").alias("filing_form"),
+        F.variant_get(F.col("observations_attr.value"),"$.fp","string").alias("fiscal_period"),
+        F.variant_get(F.col("observations_attr.value"),"$.frame","string").alias("reporting_frame"),
+        F.variant_get(F.col("observations_attr.value"),"$.fy","bigint").alias("fiscal_year"),
+        F.variant_get(F.col("observations_attr.value"),"$.val","decimal(38,10)").alias("value")
+        )
+    fields_df.printSchema()
+    fields_df.show(5, truncate=False)
 read_json("MSFT")
